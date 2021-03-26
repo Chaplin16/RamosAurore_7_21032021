876fc720661666a858ce573ebc4ea6b9fa7e659e
Chaplin16 <chaplin16@orange.fr>
2021-03-26 17:55:37 +0100

    route modify Tchat operationnelle

--- RamosAurore_7_21032021/Backend/controllers/tchat.js
+++ RamosAurore_7_21032021/Backend/controllers/tchat.js
@@ -20,31 +20,15 @@ exports.modifyTchatContent = (req, res, next) => {
     .then(content => {
         content.update({ content: req.body.content })
             .then(() => 
-                res.status(200).send({ message: 'Votre message est modifié!' }))
+                res.status(200).json({ message: 'Votre message est modifié!' }))
             .catch(error =>
-                res.status(400).send({ error }));
+                res.status(400).json({ error }));
     })
-    .catch(error =>
-        console.log(error),
-        res.status(500).send({ error: 'Problème de serveur!!' })
+    .catch(error => 
+        res.status(500).json({ error: 'Problème de serveur!!' })
     );
 };
 
-//route pour modifier le titre du tchat
-// exports.modifyTchatTitle = (req, res, next) => {
-//     const id = req.params.id  
-//     Tchat.findOne({ where: { id: id } })
-//     .then(title => {
-//         title.update({ title: req.body.title })
-//         .then(() =>
-//         res.status(200).json({ message: 'Votre titre est modifié!' }))
-//         .catch(error =>
-//             res.status(400).json({ error }));
-//         })
-//         .catch(error =>
-//             res.status(500).json({ error })
-//             );
-//         };
 
 //route pour modifier la photo
 //exports.modifyTchatAttachment = (req, res, next) => {
