b55aca7ca9d59c42245b6a99d65879fb9c30f615
Chaplin16 <chaplin16@orange.fr>
2021-03-30 23:13:19 +0200

    route comment

--- RamosAurore_7_21032021/Backend/app.js
+++ RamosAurore_7_21032021/Backend/app.js
@@ -26,6 +26,6 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/tchat', tchatRoutes);
 app.use('/', userRoutes);
-//app.use('/comment', commentRoutes);
+app.use('/comment', commentRoutes);
 
 module.exports = app; //devient accessible pour les autres fichiers
