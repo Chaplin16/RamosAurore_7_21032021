f84710b813b3e4807de3ec9ffd2843bba98826d6
Chaplin16 <chaplin16@orange.fr>
2021-04-03 02:05:33 +0200

    constructor Tchat et pageTchats.js en cours

--- RamosAurore_7_21032021/Frontend/js/pageTchats.js
+++ RamosAurore_7_21032021/Frontend/js/pageTchats.js
@@ -1,4 +1,25 @@
 let config = "http://localhost:3000";
+let myTchat = document.getElementById('myTchat');
+
+function showMessage(tchat){
+    myTchat.innerHTML += 
+        //la partie pour le message du user connecté//
+        `<button class="avatar_post"> 
+            <a href="#">Avatar</a>
+        </button>
+        <label for="tchatMainUser"> </label>
+            <input class="input_formulaire_post" 
+                type="text" 
+                name="tchatMainUser" 
+                id="tchatMainUser" 
+                placeholder="Quoi de neuf??"
+                ${tchat.content}/>
+            <a href="#" dowload="${tchat.attachment}>
+                <img class="linkImage" src="images/trombone_40.png"/>
+            </a>
+            <input type=submit id="btnTchat" class="linkSend" data-id="${tchat.id}" data-content="${tchat.content} data-image="${tchat.attachment}"  data-name="${tchat.userId}" data-create="${tchat.createAt}" data-update="${tchat.updatedAt}">
+`  
+}
 
 let allTchatsUsers = fetch(config) 
     .then(function(response) {
@@ -7,5 +28,9 @@ let allTchatsUsers = fetch(config)
     .then(function(listTchats) {
         for (let message of listMessages) {
             let tchat = new Tchats(message);
+            showMessage(tchat);
         }
+    })
+    .catch(function(err) { //le retour en cas de non connection au serveur 
+        console.log('Fetch problem: ' + err.message);
     })
