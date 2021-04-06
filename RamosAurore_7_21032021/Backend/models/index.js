cbdc22e592feabf120bcd3dafc7b59995e5e5f4c
Chaplin16 <chaplin16@orange.fr>
2021-04-06 23:11:35 +0200

    config

--- RamosAurore_7_21032021/Backend/models/index.js
+++ RamosAurore_7_21032021/Backend/models/index.js
@@ -2,15 +2,15 @@ const User = require('../models/user');
 const Tchat = require('../models/tchat');
 const Comment = require('../models/comment');
 
- User.hasMany(Tchat, { onDelete: 'cascade' });
-    
+ //User.hasMany(Tchat, { onDelete: 'cascade' });
+ Tchat.belongsTo(User);   
  Tchat.hasMany(Comment, { onDelete: 'cascade' });
  
 async function loadModel() {
     await  User.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', null)
-    await User.sync({force:true});
-    await Tchat.sync({force:true});
-    await Comment.sync({force:true});
+    await User.sync({alter:true});
+    await Tchat.sync({alter:true});
+    await Comment.sync({alter:true});
     await User.sequelize.query('SET FOREIGN_KEY_CHECKS = 1', null)
 };
 
