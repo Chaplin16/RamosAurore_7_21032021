25fdae2eb9abd347ca0a4d750ce9b4c2dc243d80
Chaplin16 <chaplin16@orange.fr>
2021-04-06 15:22:51 +0200

    tchat comment et user connecté sur la meme page pageTchats.js

---
+++ RamosAurore_7_21032021/Frontend/js/Comments.js
@@ -0,0 +1,10 @@
+//objet constructor 
+
+//je cree un objet avec le constructor  Comments
+class Comments {
+    constructor(comment) {
+        this.id = comment.id;
+        this.idUser = comment.tchatId;
+        this.comment = comment.comment;
+    }
+}
--- RamosAurore_7_21032021/Frontend/js/pageTchats.js
+++ RamosAurore_7_21032021/Frontend/js/pageTchats.js
@@ -1,35 +1,74 @@
-let config = "http://localhost:3000";
-let tchatsMembersAndComments = document.getElementById('tchatsMembersAndComments');
+const urlParams = new URL(document.location).searchParams; //recuperation du parametre (id) à la fin de l url
+const userId = urlParams.get('id');
 
-function showTchats(tchat){
-    tchatsMembersAndComments.innerHTML += 
-    `<div class="allTchats"
-        <div class="tchatMember">
-            <button class="avatarMember"> 
-                <p>${avatarMember}</p>
-            </button>
-            <p id=usernameMember class="usernameMember">${usernameMember}</p>
-            <label for="allTchatMember">${content}</label>
-            <input class="inputTchatMember" type="texterea" name="allTchatMember" id="tchatMember"/>  
-        </div>
-        <div class="commentUser">
-            <p>Commentaires</p>
-            <button class="avatarComment" id="avatarConnect"> 
-                <p>${avatar}</p>
-            </button>
-            <p id=usernameConnect class="usernameConnect">${username}</p>
-            <input class="commentUserConnect" type="texterea" name="commentUser" id="commentUserConnect"/>  
-            <img class="commentSend" src="images/fleche.png">
-        </div>
-    </div`
+//avatar et nom deu user connecté
+const avatarConnect = document.getElementById('avatarConnect');
+const usernameConnect = document.getElementById('usernameConnect');
+
+//avatar, nom et tchat des autres utilisateurs
+const allTchatsMembers = document.getElementById('allTchatsMembers');
+const commentsUsers = document.getElementById('commentsUsers')
+
+//recuperation de l avatar et du nom du user connecté
+fetch('http://localhost:3000' + '/' + userId, {
+    method: "get",
+    headers: { "Content-Type": "application/json;charset=UTF-8" },
+    mode: "cors"
+}) .then(function (response) {
+    return response.json()
+
+}).then(function(json) { 
+    let avatar =  json.avatar;
+    let username = json.username;
+
+    avatarConnect.innerHTML = ` <p>${avatar}</p> `
+    usernameConnect.innerHTML =`<span>${username}</span>`
+
+
+}) .catch(function(err) {
+    console.log('Retour info Api problem: ' + err.message);
+})
+
+
+function showTchats(tchat)
+{
+    let url = "pageTchats.html?id=" + tchat.id;
+    allTchats.innerHTML += 
+       `<div class="allTchats" id="allTchats">
+            <div class="tchatMember">
+                <button class="avatarMember"> 
+                    <p>${avatarMember}</p>
+                </button>
+                <p id=usernameMember class="usernameMember">${usernameMember}</p>
+                <label for="allTchatMember">${tchat.content}</label>
+                <input class="inputTchatMember" type="texterea" name="allTchatMember" id="tchatMember"/>  
+            </div>
+        </div>`
+}
+
+function showComments(comment)
+{
+    commentsUsers.innerHTML +=
+        `<p>Commentaires</p>
+        <button class="avatarComment" id="avatarConnect"> 
+            <p>${avatar}</p>
+        </button>
+        <p id="usernameConnect" class="usernameConnect">${username}</p>
+        <label for="commentUser">${comment}</label>
+        <input class="commentUserConnect" type="texterea" name="commentUser" id="commentUserConnect"/>  
+        <img class="commentSend" src="images/fleche.png">`
 }      
 
-fetch('http://localhost:3000' + '/' + chat + '/:id')
-    .then(function(response) {
+
+fetch('http://localhost:3000' + '/tchat/allTchats', {
+    method: "get",
+    headers: { "Content-Type": "application/json;charset=UTF-8" },
+    mode: "cors"
+})  .then(function(response) {
         return response.json();
     }) 
     .then(function(listTchats) {
-        for (let message of listMessages) {
+        for (let message of listTchats) {
             let tchat = new Tchats(message);
             showTchats(tchat);
         }
@@ -37,3 +76,4 @@ fetch('http://localhost:3000' + '/' + chat + '/:id')
     .catch(function(err) { //le retour en cas de non connection au serveur 
         console.log('Fetch problem: ' + err.message);
     })
+
