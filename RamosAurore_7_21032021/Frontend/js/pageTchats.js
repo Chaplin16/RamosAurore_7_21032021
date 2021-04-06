3ae8cc9e6c7e2b4b8ff8592180119e45155c0fce
Chaplin16 <chaplin16@orange.fr>
2021-04-06 14:09:59 +0200

    constructor Users

--- RamosAurore_7_21032021/Frontend/js/pageTchats.js
+++ RamosAurore_7_21032021/Frontend/js/pageTchats.js
@@ -6,32 +6,34 @@ function showTchats(tchat){
     `<div class="allTchats"
         <div class="tchatMember">
             <button class="avatarMember"> 
-                <a href="#">Avatar</a>
+                <p>${avatarMember}</p>
             </button>
-            <label for="allTchatMember"> </label>
+            <p id=usernameMember class="usernameMember">${usernameMember}</p>
+            <label for="allTchatMember">${content}</label>
             <input class="inputTchatMember" type="texterea" name="allTchatMember" id="tchatMember"/>  
         </div>
         <div class="commentUser">
-            <p>X Commentaires</p>
-            <button class="avatarComment"> 
-                <a href="#">Avatar</a>
+            <p>Commentaires</p>
+            <button class="avatarComment" id="avatarConnect"> 
+                <p>${avatar}</p>
             </button>
+            <p id=usernameConnect class="usernameConnect">${username}</p>
             <input class="commentUserConnect" type="texterea" name="commentUser" id="commentUserConnect"/>  
             <img class="commentSend" src="images/fleche.png">
         </div>
     </div`
 }      
 
-// fetch(config + '/' + chat + '/' + allTchats)
-//     .then(function(response) {
-//         return response.json();
-//     }) 
-//     .then(function(listTchats) {
-//         for (let message of listMessages) {
-//             let tchat = new Tchats(message);
-//             showTchats(tchat);
-//         }
-//     })
-//     .catch(function(err) { //le retour en cas de non connection au serveur 
-//         console.log('Fetch problem: ' + err.message);
-//     })
+fetch('http://localhost:3000' + '/' + chat + '/:id')
+    .then(function(response) {
+        return response.json();
+    }) 
+    .then(function(listTchats) {
+        for (let message of listMessages) {
+            let tchat = new Tchats(message);
+            showTchats(tchat);
+        }
+    })
+    .catch(function(err) { //le retour en cas de non connection au serveur 
+        console.log('Fetch problem: ' + err.message);
+    })
